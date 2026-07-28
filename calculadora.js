8da2efb247127bea2eddccab9d83512771ed07b1
fernandoantonio2552-ship-it <fernandoantonio2552@gmail.com>
2026-07-28 18:26:17 -0500

    Primer Commit del proyecto de Auditoria  de Calidad de Código

--- calculadora.js
+++ calculadora.js
@@ -1,12 +1,21 @@
 function sumar(a, b) {
-  return a + b;
+    let resultado = a + b;
+    return resultado;
 }
 
 function dividir(a, b) {
-  if (b === 0) {
-    throw new Error("No se puede dividir entre cero");
-  }
-  return a / b;
+    if (b === 0) {
+        throw new Error("No se puede dividir entre cero");
+    }
+    return a / b;
 }
 
-module.exports = { sumar, dividir };
+function contraseñaAdmin() {
+    const password = "123456";
+    console.log(password);
+}
+
+module.exports = {
+    sumar,
+    dividir
+};
